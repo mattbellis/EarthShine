a7744a0ce645a2a6bafeb1d28a9363daa137bb31
Matt Bellis <matthew.bellis@gmail.com>
2026-03-17 02:27:20 -0400

    Stuff

--- background_and_signal_acceptance_studies/GEN_EVENTS.sh
+++ background_and_signal_acceptance_studies/GEN_EVENTS.sh
@@ -1,3 +1,11 @@
+python generate_signal_events_CL.py --masses-a 0.22 --masses-dm 20000 \
+                                    --nevents 1_000_000 --ntrials 20 \
+                                    --depth -8 \
+                                    --save-to-file \
+                                    --output-directory OUTPUT_FILES
+                                    --average-eloss \
+                                    --dm-model momentum_constrained  \
+
 #python generate_signal_events_CL.py --masses-a 0.22 --masses-dm 20000 \
                                     #--nevents 1_000_000 --ntrials 20 \
                                     #--depth -8 -6000 --disk-radius 40 \
@@ -7,32 +15,32 @@
                                     #--average-eloss \
                                     #--dm-model momentum_constrained  \
 
-python generate_signal_events_CL.py --masses-a 0.22 --masses-dm 7000 \
-                                    --nevents 1_000_000 --ntrials 5000 \
-                                    --depth -8 -3500 --disk-radius 3500 \
-                                    --dm-model floating  \
-                                    --save-to-file \
-                                    --output-directory OUTPUT_FILES
-
-python generate_signal_events_CL.py --masses-a 0.22 --masses-dm 7000 \
-                                    --nevents 1_000_000 --ntrials 5000 \
-                                    --depth -8 -3500 --disk-radius 3500 \
-                                    --dm-model momentum_constrained  \
-                                    --save-to-file \
-                                    --output-directory OUTPUT_FILES
-
-python generate_signal_events_CL.py --masses-a 0.22 --masses-dm 7000 \
-                                    --nevents 1_000_000 --ntrials 5000 \
-                                    --depth -8 -3500 --disk-radius 3500 \
-                                    --dm-model floating  \
-                                    --save-to-file \
-                                    --output-directory OUTPUT_FILES \
-                                    --average-eloss 
-
-python generate_signal_events_CL.py --masses-a 0.22 --masses-dm 7000 \
-                                    --nevents 1_000_000 --ntrials 5000 \
-                                    --depth -8 -3500 --disk-radius 3500 \
-                                    --dm-model momentum_constrained  \
-                                    --save-to-file \
-                                    --output-directory OUTPUT_FILES \
-                                    --average-eloss 
+#python generate_signal_events_CL.py --masses-a 0.22 --masses-dm 7000 \
+                                    #--nevents 1_000_000 --ntrials 5000 \
+                                    #--depth -8 -3500 --disk-radius 3500 \
+                                    #--dm-model floating  \
+                                    #--save-to-file \
+                                    #--output-directory OUTPUT_FILES
+#
+#python generate_signal_events_CL.py --masses-a 0.22 --masses-dm 7000 \
+                                    #--nevents 1_000_000 --ntrials 5000 \
+                                    #--depth -8 -3500 --disk-radius 3500 \
+                                    #--dm-model momentum_constrained  \
+                                    #--save-to-file \
+                                    #--output-directory OUTPUT_FILES
+#
+#python generate_signal_events_CL.py --masses-a 0.22 --masses-dm 7000 \
+                                    #--nevents 1_000_000 --ntrials 5000 \
+                                    #--depth -8 -3500 --disk-radius 3500 \
+                                    #--dm-model floating  \
+                                    #--save-to-file \
+                                    #--output-directory OUTPUT_FILES \
+                                    #--average-eloss 
+#
+#python generate_signal_events_CL.py --masses-a 0.22 --masses-dm 7000 \
+                                    #--nevents 1_000_000 --ntrials 5000 \
+                                    #--depth -8 -3500 --disk-radius 3500 \
+                                    #--dm-model momentum_constrained  \
+                                    #--save-to-file \
+                                    #--output-directory OUTPUT_FILES \
+                                    #--average-eloss 
